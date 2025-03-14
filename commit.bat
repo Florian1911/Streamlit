eb8dc3bb12fdf49231be90300040a2934de90560
Florian1911 <222439@umons.ac.be>
2025-03-14 10:50:12 +0100

    Update

--- commit.bat
+++ commit.bat
@@ -3,4 +3,4 @@ git remote add origin https://github.com/Florian1911/Streamlit.git
 git remote set-url origin https://github.com/Florian1911/Streamlit.git
 git add .
 git commit -m "Update"
-git push -u origin main
+git push -u origin master
