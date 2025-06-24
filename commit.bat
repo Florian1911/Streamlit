bae32376d2050c7e3c9466459f08188e8109dada
Florian1911 <222439@umons.ac.be>
2025-06-24 16:36:56 +0200

    Delete commit.bat

--- commit.bat
+++
@@ -1,6 +0,0 @@
-git init
-git remote add origin https://github.com/Florian1911/Streamlit.git
-git remote set-url origin https://github.com/Florian1911/Streamlit.git
-git add .
-git commit -m "Update"
-git push -u origin master
