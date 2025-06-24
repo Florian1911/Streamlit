e721bafea20db19154aa4ef2d0622d3affdc2c61
Florian1911 <222439@umons.ac.be>
2025-06-24 16:36:12 +0200

    Delete run_mag1.bat

--- run_mag1.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Users\florian1911\AppData\Local\anaconda3\Scripts\activate.bat" mystreamlit
-cd /d "C:\Users\florian1911\Documents\Streamlit\Loudness"
-streamlit run "Loudness.py"
-pause
